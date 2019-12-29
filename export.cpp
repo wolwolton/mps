f3452515d08a054d31c8227b798993e4948e5f0d
wolwolton <yusakuwithniko@gmail.com>
2019-12-29 15:35:20 +0900

    fix vtu file

--- export.cpp
+++ export.cpp
@@ -29,7 +29,7 @@ int Export::exportPara(const std::string name, const std::vector<Particle> &pcls
     ofs << "</DataArray>" << std::endl;
     ofs << "<DataArray NumberOfComponents='1' type='Float32' Name='Velocity' format='ascii'>" << std::endl;
     for(auto pcl  : pcls){
-        ofs << pcl.vel.norm();
+        ofs << pcl.vel.norm() << std::endl;
     }
     ofs << "</DataArray>" << std::endl;
     ofs << "<DataArray NumberOfComponents='1' type='Float32' Name='pressure' format='ascii'>" << std::endl;
