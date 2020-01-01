3e161d6d9aeebba463ad279869a1f40d4c2cfeb6
wolwolton <yusakuwithniko@gmail.com>
2020-01-01 11:50:46 +0900

    change sample model dimension

--- mk_particle.cpp
+++ mk_particle.cpp
@@ -4,13 +4,13 @@
 #include "particle.h"
 
 void MakeParticle::make_bar(std::vector<Particle> &pcls){
-    std::vector<Particle> pcls_tmp(70*20*40);
+    std::vector<Particle> pcls_tmp(70*40);
     pcls = pcls_tmp;
     float pcl_dist = 0.025;
     for(int ix=0; ix<70; ix++){
-        for(int iy=0; iy<20; iy++){
+        for(int iy=0; iy<1; iy++){
             for(int iz=0; iz<40; iz++){
-                int idx = ix*20*40+iy*40+iz;
+                int idx = ix*1*40+iy*40+iz;
                 pcls[idx].pos=pcl_dist*(Eigen::Vector3f::UnitX()*(float)ix
                                        +Eigen::Vector3f::UnitY()*(float)iy
                                        +Eigen::Vector3f::UnitZ()*(float)iz);
@@ -21,19 +21,19 @@ void MakeParticle::make_bar(std::vector<Particle> &pcls){
         }
     }
     for(int ix=0; ix<70; ix++){
-        for(int iy=0; iy<20; iy++){
+        for(int iy=0; iy<1; iy++){
             for(int iz=0; iz<40; iz++){
                 int typ_tmp = Particle::GST;
-                if(3<ix && ix < 14 && 3<iy && iy<15 && 3<iz && iz < 30){
+                if(3<ix && ix < 14  && 3<iz && iz < 30){
                     typ_tmp = Particle::FLD;
                 }
-                if(iz<4 || (iz<30 && (ix<4 || 65<ix || iy<4 || 15<iy))){
+                if(iz<4 || (iz<30 && (ix<4 || 65<ix ))){
                     typ_tmp = Particle::DMY;
                 }
-                if(iz<2 || (iz<30 && (ix<2 || 67<ix || iy<2 || 17<iy))){
+                if(iz<2 || (iz<30 && (ix<2 || 67<ix ))){
                     typ_tmp = Particle::WLL;
                 }
-                pcls[ix*20*40+iy*40+iz].typ=typ_tmp;
+                pcls[ix*1*40+iy*40+iz].typ=typ_tmp;
             }
         }
     }
